f9ca3f2dd3be0735a45b54252f15e4faf229d631
GW <kowonp@gmail.com>
2022-03-03 02:42:40 +0900

    Create Drag and Drop list using react-beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,92 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { filteredMinutesState, millisecondsState, minutesState, secondsState } from "./atom";
+import {
+  DragDropContext,
+  Draggable,
+  DraggableProvided,
+  DraggableStateSnapshot,
+  DragStart,
+  Droppable,
+  DroppableProvided,
+  DroppableStateSnapshot,
+  DropResult,
+  ResponderProvided,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { todosState } from "./atom";
 import GlobalStyle from "./styles/GlobalStyle";
 
+const Container = styled.div`
+  border: 3px solid yellow;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
+`;
+
+const Board = styled.div`
+  background-color: ${(props) => props.theme.boardColor};
+  padding: 20px;
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
 const App = () => {
-  const [minutes, setMinutes] = useRecoilState<number>(minutesState);
-  const [hours, setHours] = useRecoilState<number>(filteredMinutesState);
-  const seconds = useRecoilValue<number>(secondsState);
-  const milliseconds = useRecoilValue<number>(millisecondsState);
+  const [todos, setTodos] = useRecoilState(todosState);
 
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>): void => {
-    setMinutes(+event.currentTarget.value);
-  };
+  const onDragStart = (initial: DragStart, provided: ResponderProvided) => {};
 
-  const onHoursChange = (event: React.FormEvent<HTMLInputElement>): void => {
-    setHours(+event.currentTarget.value);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult, provided: ResponderProvided) => {
+    console.log("draggableId", draggableId, "destination?.index", destination?.index, "source.index", source.index);
+    if (destination?.index === undefined) return;
+
+    setTodos((todos) => {
+      const copiedTodos = [...todos];
+      copiedTodos.splice(source.index, 1);
+      copiedTodos.splice(destination.index, 0, draggableId);
+      return copiedTodos;
+    });
   };
 
   return (
-    <div>
+    <Container>
       <GlobalStyle />
-      <form>
-        <div>
-          Hours: <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
-        </div>
-        <div>
-          Minutes: <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-        </div>
-        <div>
-          Seconds: <input value={seconds} readOnly type="number" placeholder="Hours" />
-        </div>
-        <div>
-          Milliseconds: <input value={milliseconds} readOnly type="number" placeholder="Hours" />
-        </div>
-      </form>
-    </div>
+      <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
+        <Boards>
+          <Droppable droppableId="one">
+            {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+              <Board ref={provided.innerRef} {...provided.droppableProps}>
+                {todos.map((todo, index) => (
+                  <Draggable draggableId={todo} index={index} key={todo}>
+                    {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+                      <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        {todo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </DragDropContext>
+    </Container>
   );
 };
 
--- src/atom.ts
+++ src/atom.ts
@@ -1,19 +1,3 @@
-import { atom, DefaultValue, RecoilState, selector } from "recoil";
+import { atom } from "recoil";
 
-export const minutesState: RecoilState<number> = atom({ key: "minutesState", default: 0 });
-export const secondsState: RecoilState<number> = atom({ key: "secondsState", default: 0 });
-export const millisecondsState: RecoilState<number> = atom({ key: "millisecondsState", default: 0 });
-
-export const filteredMinutesState: RecoilState<number> = selector({
-  key: "filteredMinutesState",
-  get: ({ get }): number => {
-    const minutes: number = get(minutesState);
-    const hours: number = minutes / 60;
-    return hours;
-  },
-  set: ({ set }, newValue: number | DefaultValue): void => {
-    set(minutesState, Number(newValue) * 60);
-    set(secondsState, Number(newValue) * 3600);
-    set(millisecondsState, Number(newValue) * 3600000);
-  },
-});
+export const todosState = atom({ key: "todosState", default: ["study", "coding", "eat", "dance", "play"] });

dc298040a87eb91a22b3efe14f74920da846c420
GW <kowonp@gmail.com>
2022-03-06 22:05:11 +0900

    Droppable Snapshot, Draggable Snapshot

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -7,21 +7,25 @@ interface DraggableCardProps {
   todo: string;
 }
 
-const Card = styled.div`
-  background-color: ${(props) => props.theme.cardColor};
+const Card = styled.div<{ isDragging: boolean }>`
+  background-color: ${(props) => (props.isDragging === true ? "rgba(116, 185, 255,0.8)" : props.theme.cardColor)};
+  color: ${(props) => (props.isDragging === true ? "white" : "black")};
   border-radius: 5px;
-  padding: 12px 10px;
+  padding: 15px 12px;
   margin-bottom: 10px;
+  font-size: 20px;
 `;
 
 const DraggableCard = ({ index, todo }: DraggableCardProps) => {
   return (
     <Draggable index={index} draggableId={todo} key={todo}>
-      {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-        <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          {todo}
-        </Card>
-      )}
+      {(provided: DraggableProvided, { isDragging }: DraggableStateSnapshot) => {
+        return (
+          <Card isDragging={isDragging} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            {todo}
+          </Card>
+        );
+      }}
     </Draggable>
   );
 };
--- src/components/DroppableBoard.tsx
+++ src/components/DroppableBoard.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Droppable, DroppableProvided, DroppableStateSnapshot } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
@@ -9,7 +10,7 @@ interface DroppableBoardProps {
 
 const Board = styled.div`
   background-color: ${(props) => props.theme.boardColor};
-  padding: 20px;
+  padding: 25px 10px;
   border-radius: 5px;
   min-height: 200px;
 `;
@@ -21,19 +22,42 @@ const BoardId = styled.h1`
   margin-bottom: 13px;
 `;
 
+const BoardContent = styled.div<{ isDraggingOver: boolean; draggingFromThisWith: boolean }>`
+  height: calc(100% - 30px);
+  border-radius: 5px;
+  transition: all 0.5s;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: ${(props) =>
+    props.isDraggingOver === true ? "rgba(178, 190, 195,0.5)" : props.draggingFromThisWith === true ? "rgba(231, 76, 60,0.8)" : props.theme.boardColor};
+`;
+
 const DroppableBoard = ({ boardId, todos }: DroppableBoardProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    console.log("inputRef", inputRef);
+    inputRef.current?.focus();
+  };
+
   return (
-    <Droppable droppableId={boardId}>
-      {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-        <Board ref={provided.innerRef} {...provided.droppableProps}>
-          <BoardId>{boardId}</BoardId>
-          {todos.map((todo: string, index: number) => (
-            <DraggableCard key={todo} index={index} todo={todo} />
-          ))}
-          {provided.placeholder}
-        </Board>
-      )}
-    </Droppable>
+    <div>
+      <input ref={inputRef} type="text" />
+      <button onClick={handleClick}>Click</button>
+      <Droppable droppableId={boardId}>
+        {(provided: DroppableProvided, { isDraggingOver, draggingOverWith, draggingFromThisWith, isUsingPlaceholder }: DroppableStateSnapshot) => (
+          <Board ref={provided.innerRef} {...provided.droppableProps}>
+            <BoardId>{boardId}</BoardId>
+            <BoardContent isDraggingOver={isDraggingOver} draggingFromThisWith={Boolean(draggingFromThisWith)}>
+              {todos.map((todo: string, index: number) => (
+                <DraggableCard key={todo} index={index} todo={todo} />
+              ))}
+              {provided.placeholder}
+            </BoardContent>
+          </Board>
+        )}
+      </Droppable>
+    </div>
   );
 };
 

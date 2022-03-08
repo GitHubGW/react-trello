ab8f320787ca6640da77a1ecbeb3caaa410c71eb
GW <kowonp@gmail.com>
2022-03-08 20:34:04 +0900

    feat(DroppableBoard): create todo

--- src/components/DroppableBoard.tsx
+++ src/components/DroppableBoard.tsx
@@ -1,11 +1,17 @@
-import { useRef } from "react";
 import { Droppable, DroppableProvided, DroppableStateSnapshot } from "react-beautiful-dnd";
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { Todo, TodosState, todosState } from "../atom";
 import DraggableCard from "./DraggableCard";
 
 interface DroppableBoardProps {
   boardId: string;
-  todos: any;
+  todos: Todo[];
+}
+
+interface FormData {
+  text: string;
 }
 
 const Board = styled.div`
@@ -32,25 +38,36 @@ const BoardContent = styled.div<{ isDraggingOver: boolean; draggingFromThisWith:
     props.isDraggingOver === true ? "rgba(178, 190, 195,0.5)" : props.draggingFromThisWith === true ? "rgba(231, 76, 60,0.8)" : props.theme.boardColor};
 `;
 
-const DroppableBoard = ({ boardId, todos }: DroppableBoardProps) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const Form = styled.form``;
 
-  const handleClick = () => {
-    console.log("inputRef", inputRef);
-    inputRef.current?.focus();
+const Input = styled.input``;
+
+const DroppableBoard = ({ boardId, todos }: DroppableBoardProps) => {
+  const { register, handleSubmit, setValue, getValues } = useForm<FormData>({ mode: "onChange", defaultValues: { text: "" } });
+  const setTodos = useSetRecoilState(todosState);
+
+  const onValid = (): void => {
+    setTodos((todos: TodosState) => {
+      const { text } = getValues();
+      const createdTodo: Todo = { id: Date.now(), text };
+      const result = { ...todos, [boardId]: [createdTodo, ...todos[boardId]] };
+      return result;
+    });
+    setValue("text", "");
   };
 
   return (
     <div>
-      <input ref={inputRef} type="text" />
-      <button onClick={handleClick}>Click</button>
+      <Form onSubmit={handleSubmit(onValid)}>
+        <Input {...register("text", { required: "할 일을 입력하세요." })} type="text" placeholder={`할 일을 추가하세요.`} />
+      </Form>
       <Droppable droppableId={boardId}>
         {(provided: DroppableProvided, { isDraggingOver, draggingOverWith, draggingFromThisWith, isUsingPlaceholder }: DroppableStateSnapshot) => (
           <Board ref={provided.innerRef} {...provided.droppableProps}>
             <BoardId>{boardId}</BoardId>
             <BoardContent isDraggingOver={isDraggingOver} draggingFromThisWith={Boolean(draggingFromThisWith)}>
-              {todos.map((todo: string, index: number) => (
-                <DraggableCard key={todo} index={index} todo={todo} />
+              {todos.map((todo: Todo, index: number) => (
+                <DraggableCard key={todo.id} index={index} boardId={boardId} todoId={todo.id} todoText={todo.text} />
               ))}
               {provided.placeholder}
             </BoardContent>
